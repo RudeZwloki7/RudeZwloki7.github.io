3656c453069a688275e1f0d42722b5601a4de686
Simon <rudezwloki7@gmail.com>
2020-12-07 03:26:19 +0300

    Working with JS

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#element").fadeOut("slow", function (){
+        console.log("done")
+    });
+});
+// window.onload = function() {
+//     $('#element').fadeOut('slow'); //fades in the text
+// };

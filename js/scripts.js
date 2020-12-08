2dcf6bc8aa43618d774cc6ebe01d9553904992dd
anikii <rudezwloki7@gmail.com>
2020-12-08 18:20:11 +0400

    Wrote scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-    $("#element").fadeOut("slow", function (){
-        console.log("done")
+    $('#header'). fadeOut(1);
+    $('#info').fadeOut(1);
+    $('#containment').fadeOut(1);
+    $('#header').fadeIn(1000,function (){
+        $('.dropdown-divider').fadeIn(1000,function () {
+            $('#info').fadeIn(1000,function (){
+                $('#containment').fadeIn(1000)
+            })
+        })
     });
 });
-// window.onload = function() {
-//     $('#element').fadeOut('slow'); //fades in the text
-// };
+
+$(document).ready(function (){
+    $('.card-elem').hover(
+        function(){$(this).animate({marginTop: '-=2%'}, 200);},
+        function(){$(this).animate({marginTop: '0'}, 200);}
+    );
+});
